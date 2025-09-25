0ae132803a0342fb3a1f86171702aca72ebde803
Mohamed Yassine Landolsi <yesslandolsi@gmail.com>
2025-09-25 13:54:47 +0100

    update checkout page

--- frontend/src/routes/app/store/checkout.tsx
+++ frontend/src/routes/app/store/checkout.tsx
@@ -1,5 +1,7 @@
+import type { Product } from '@/api/stores';
 import { MobileContainer } from '@/components';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { X } from 'lucide-react';
 
 export const Route = createFileRoute('/app/store/checkout')({
   component: RouteComponent,
@@ -7,51 +9,67 @@ export const Route = createFileRoute('/app/store/checkout')({
 
 function RouteComponent() {
   return (
+    <div className="justify- mx-auto flex min-h-screen flex-col items-center">
+      <ProductCheckout
+        product={{
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString(),
+          clickToPay: 'Buy now',
+          isPublished: true,
+          displayOrder: 1,
+          productType: 'Session',
+          storeSlug: 'sophia-carter-store',
+          productSlug: 'ultimate-guide-design',
+          title: 'The Ultimate Guide to Design',
+          subtitle: 'Master the art of modern UI/UX',
+          description:
+            "This is a detailed description of the product, highlighting its features, benefits, and any other relevant information that would help a customer make a purchase decision. It's designed to be comprehensive yet easy to read. Dive deep into principles of visual hierarchy, color theory, and interaction design.",
+          price: 49.0,
+
+          thumbnail: {
+            mainLink:
+              'https://lh3.googleusercontent.com/aida-public/AB6AXuA7k7Y1yTUsoGw4kHQ-au2cb85NtWAxnzb8R4sAvMlM73WrWUrUhuzqa4i7fvFJkbZWWSamtgVPoY-LtYj2gEnfzCm_dM2sMbL8X7I9DyaGB-5lL6f2r-p6sHYczA-ID-kqquaLSMCTtOhr2ykI8_fET6STytxU4CjxNKoOZN_VDA-lcAdOBxwmj8rzTr0mciZU8q_VMQfWb5WYSZO7zwbSrPShrVxxD30-i6ZZsBXgQjO5wAHROE-LgHbSv_-CIKLF4K2o7rbJB-Zs',
+            thumbnailLink: '',
+          },
+        }}
+      />
+    </div>
+  );
+}
+
+export function ProductCheckout({ product }: { product: Product }) {
+  return (
     <MobileContainer>
       <div className="bg-background-light dark:bg-background-dark relative min-h-screen text-slate-800 dark:text-slate-200">
         <header className="absolute top-0 right-0 left-0 z-10 flex items-center justify-between p-4">
-          <a className="flex items-center gap-2" href="#">
-            <span className="bg-primary/20 dark:bg-primary/30 flex h-6 w-6 items-center justify-center rounded-full">
-              <svg className="text-primary h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  clip-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                  fill-rule="evenodd"
-                ></path>
-              </svg>
-            </span>
+          <Link className="flex items-center gap-2" to={'/app/store/builder'}>
+            <X />
             <span className="font-bold text-slate-900 dark:text-white">My Store</span>
-          </a>
+          </Link>
         </header>
         <main className="pb-28">
-          <div className="h-80 w-full">
-            <img
-              alt="Product Image"
-              className="h-full w-full object-cover"
-              src="https://lh3.googleusercontent.com/aida-public/AB6AXuA7k7Y1yTUsoGw4kHQ-au2cb85NtWAxnzb8R4sAvMlM73WrWUrUhuzqa4i7fvFJkbZWWSamtgVPoY-LtYj2gEnfzCm_dM2sMbL8X7I9DyaGB-5lL6f2r-p6sHYczA-ID-kqquaLSMCTtOhr2ykI8_fET6STytxU4CjxNKoOZN_VDA-lcAdOBxwmj8rzTr0mciZU8q_VMQfWb5WYSZO7zwbSrPShrVxxD30-i6ZZsBXgQjO5wAHROE-LgHbSv_-CIKLF4K2o7rbJB-Zs"
-            />
+          <div className="bg-accent flex h-80 w-full items-center justify-center">
+            {/* Change to lazy Image */}
+            <img alt="Product Image" className="h-full w-full object-cover" src={product.thumbnail?.mainLink} />
           </div>
           <div className="space-y-5 p-6">
-            <h1 className="text-3xl font-extrabold text-slate-900 dark:text-white">The Ultimate Guide to Design</h1>
-            <p className="mt-2 text-lg text-slate-600 dark:text-slate-400">Master the art of modern UI/UX</p>
+            <h1 className="text-3xl font-extrabold text-slate-900 dark:text-white">{product.title}</h1>
+            <p className="mt-2 text-lg text-slate-600 dark:text-slate-400">{product.subtitle}</p>
             <div className="mt-4">
-              <span className="text-primary text-4xl font-bold">$49</span>
+              <span className="text-primary text-4xl font-bold">{product.price.toFixed(2)}</span>
             </div>
             <div className="mt-6 space-y-4 text-slate-700 dark:text-slate-300">
               <h2 className="text-xl font-bold text-slate-900 dark:text-white">Description</h2>
-              <p>
-                This is a detailed description of the product, highlighting its features, benefits, and any other relevant information that would help
-                a customer make a purchase decision. It's designed to be comprehensive yet easy to read.
-              </p>
-              <p>Dive deep into principles of visual hierarchy, color theory, and interaction design.</p>
+              <p>{product.description}</p>
             </div>
-            <div className="bg-primary/10 dark:bg-primary/20 mt-6 rounded-lg p-4">
+            {/* <div className="bg-primary/10 dark:bg-primary/20 mt-6 rounded-lg p-4">
               <h3 className="font-bold text-slate-900 dark:text-white">Fulfillment Note</h3>
-              <p className="mt-1 text-sm text-slate-700 dark:text-slate-300">
-                A download link for the digital product will be sent to your email address immediately after purchase.
-              </p>
-            </div>
-            <button className="bg-primary shadow-primary/30 hover:bg-opacity-90 h-14 w-full rounded-xl text-lg font-bold text-white shadow-lg transition-all duration-300">
+              <p className="mt-1 text-sm text-slate-700 dark:text-slate-300">{fulfillmentNote}</p>
+            </div> */}
+            <button
+              onClick={() => alert('Purchase completed!')}
+              className="bg-primary shadow-primary/30 hover:bg-opacity-90 h-14 w-full rounded-xl text-lg font-bold text-white shadow-lg transition-all duration-300"
+            >
               Buy Now
             </button>
           </div>
