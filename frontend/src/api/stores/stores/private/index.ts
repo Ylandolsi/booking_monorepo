c465be90604f13b7f5b4c6b6722c4b320c8fff88
Mohamed Yassine Landolsi <yesslandolsi@gmail.com>
2025-09-26 14:01:55 +0100

    remove depricated file

--- frontend/src/api/stores/stores/private/index.ts
+++ frontend/src/api/stores/stores/private/index.ts
@@ -2,5 +2,4 @@ export * from './check-slug-availability';
 export * from './create-store';
 export * from './get-mystore';
 export * from './update-store';
-export * from './__depricated/update-store-picture';
 export * from './store-schema';
