82da28e5d45394bd6f4665921acde46f3b9ce7a4
Mohamed Yassine Landolsi <yesslandolsi@gmail.com>
2025-09-28 16:02:07 +0100

    updata auth-endpoitns : remove depricated endpoints

--- frontend/src/api/utils/auth-endpoints.ts
+++ frontend/src/api/utils/auth-endpoints.ts
@@ -17,33 +17,6 @@ const IntegrateKonnectWallet = 'users/integrate/konnect';
 const GetUser = 'users/{userSlug}';
 const GetCurrentUser = 'users/me';
 
-// Experience
-const GetUserExperiences = 'users/experiences{userSlug}';
-const AddExperience = 'users/experiences';
-const UpdateExperience = 'users/experiences/{experienceId}';
-const DeleteExperience = 'users/experiences/{experienceId}';
-
-// Education
-const GetUserEducations = 'users/educations/{userSlug}';
-const AddEducation = 'users/educations';
-const UpdateEducation = 'users/education/{educationId}';
-const DeleteEducation = 'users/education/{educationId}';
-
-// Expertise
-const GetUserExpertises = 'users/expertises/{userSlug}';
-const GetAllExpertises = 'expertises';
-const UpdateUserExpertise = 'users/expertises';
-
-// Language
-const GetUserLanguages = 'users/languages/{userSlug}';
-const GetAllLanguages = 'languages';
-const UpdateUserLanguages = 'users/languages';
-
-// Profile
-const UpdateBasicInfo = 'users/profile/basic-info';
-const UpdateProfilePicture = 'users/profile/picture';
-const UpdateSocialLinks = 'users/profile/social-links';
-
 export {
   GoogleLogin,
   GoogleLoginCallback,
@@ -58,22 +31,5 @@ export {
   ResetPasword as ResetPasswordVerify,
   GetCurrentUser,
   GetUser,
-  GetUserExperiences,
   IntegrateKonnectWallet,
-  AddExperience,
-  UpdateExperience,
-  DeleteExperience,
-  GetUserEducations,
-  AddEducation,
-  UpdateEducation,
-  DeleteEducation,
-  GetUserExpertises,
-  GetAllExpertises,
-  UpdateUserExpertise,
-  GetUserLanguages,
-  GetAllLanguages,
-  UpdateUserLanguages,
-  UpdateBasicInfo,
-  UpdateProfilePicture,
-  UpdateSocialLinks,
 };
