768d3f1738eace85849bceb0680301853d43eccb
Mohamed Yassine Landolsi <yesslandolsi@gmail.com>
2025-10-04 21:59:54 +0100

    cleanup images

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,11 +1,9 @@
 import FALLBACK_SESSION_PRODUCT_PICTURE_THUMBNAIL from './fallback-session-product-thumbnail.jpg';
 import IPHONE_MOCKUP from './apple-iphone-16-pro-max-2024-medium.png';
 import FALLBACK_PICTURE_STORE from './fallback.jpg';
-import LOGO_WEBP from './logo.webp';
-import LOGO_PNG from './logo.png';
 
 import LOGO_FULL from './logo_full.png';
 import LOGO_SHORT from './logo_short.png';
 
-export { LOGO_FULL, LOGO_SHORT, LOGO_PNG, LOGO_WEBP, FALLBACK_SESSION_PRODUCT_PICTURE_THUMBNAIL, IPHONE_MOCKUP, FALLBACK_PICTURE_STORE };
+export { LOGO_FULL, LOGO_SHORT, FALLBACK_SESSION_PRODUCT_PICTURE_THUMBNAIL, IPHONE_MOCKUP, FALLBACK_PICTURE_STORE };
 // export { LOGO_FULL, LOGO_SHORT };
