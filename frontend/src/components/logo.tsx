4b4e64099255e0b4fc84555ecf755cd4d317073b
Mohamed Yassine Landolsi <yesslandolsi@gmail.com>
2025-10-05 08:07:39 +0100

    small fix

--- frontend/src/components/logo.tsx
+++ frontend/src/components/logo.tsx
@@ -1,7 +1,5 @@
 import { cn } from '@/lib/cn';
-// import logo from '@/assets/logo.svg';
 import { LOGO_FULL } from '@/assets';
-
 export const Logo = ({ className }: { className?: string }) => {
   return <img className={cn('h-8 w-auto object-contain object-top', className)} src={LOGO_FULL} alt="Workflow" />;
 };
