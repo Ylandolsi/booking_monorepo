00881bbb0973f50125ca194011ee1f1123db5003
Mohamed Yassine Landolsi <yesslandolsi@gmail.com>
2025-10-06 09:33:41 +0100

    migration : add timestamp : usedfor stats aggregator

---
+++ backend/Booking/src/Modules/Booking.Modules.Catalog/Persistence/Migrations/20251006083253_TimeStampForUsedForStats.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Modules.Catalog.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeStampForUsedForStats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "stats_processed_at",
+                schema: "catalog",
+                table: "store_visits",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "stats_processed_at",
+                schema: "catalog",
+                table: "orders",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "stats_processed_at",
+                schema: "catalog",
+                table: "store_visits");
+
+            migrationBuilder.DropColumn(
+                name: "stats_processed_at",
+                schema: "catalog",
+                table: "orders");
+        }
+    }
+}
